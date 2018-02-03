ba4af79c62543921ca9d5b13d3449954da09c49f
Siavash Safi <siavash.safi@gmail.com>
2018-02-03 23:06:27 +0100

    Add tests for login1

--- login1/dbus_test.go
+++ login1/dbus_test.go
@@ -15,6 +15,8 @@
 package login1
 
 import (
+	"fmt"
+	"os/user"
 	"testing"
 )
 
@@ -24,5 +26,73 @@ func TestNew(t *testing.T) {
 
 	if err != nil {
 		t.Fatal(err)
+	}
+}
+
+func TestListSessions(t *testing.T) {
+	c, err := New()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	sessions, err := c.ListSessions()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(sessions) < 1 {
+		t.Fatal(fmt.Errorf("expected at least one active session"))
+	}
+
+	user, err := user.Current()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	found := false
+	for _, s := range sessions {
+		if fmt.Sprint(s.UID) == user.Uid {
+			found = true
+			if s.User != user.Username {
+				t.Fatal(fmt.Errorf("expected user('%s') but got user('%s')", user.Username, s.User))
+			}
+		}
+	}
+	if !found {
+		t.Fatal(fmt.Errorf("expected active session for user(%s) uid('%s')", user.Username, user.Uid))
+	}
+}
+
+func TestListUsers(t *testing.T) {
+	c, err := New()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	users, err := c.ListUsers()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(users) < 1 {
+		t.Fatal(fmt.Errorf("expected at least one active user"))
+	}
+
+	user, err := user.Current()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	found := false
+	for _, u := range users {
+		if fmt.Sprint(u.UID) == user.Uid {
+			found = true
+			if u.Name != user.Username {
+				t.Fatal(fmt.Errorf("expected user('%s') but got user('%s')", user.Username, u.Name))
+			}
+		}
+	}
+	if !found {
+		t.Fatal(fmt.Errorf("expected active user session for uid 1000"))
 	}
 }
